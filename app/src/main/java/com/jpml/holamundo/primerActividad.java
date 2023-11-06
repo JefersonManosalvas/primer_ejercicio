40dd508f638675faa45a867bb18d1e81fee70d71
JefersonManosalvas <132539368+JefersonManosalvas@users.noreply.github.com>
2023-11-06 13:12:40 -0500

    hola mundo

---
+++ app/src/main/java/com/jpml/holamundo/primerActividad.java
@@ -0,0 +1,14 @@
+package com.jpml.holamundo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class primerActividad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_primer_actividad);
+    }
+}
